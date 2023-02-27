712876867e9058ed0c8eb6145f2b69fe9139922f
Jeremias Jalil <83228268+jeremias-jalil@users.noreply.github.com>
2023-02-27 10:58:27 +0000

    Fix cors error

--- api/app.js
+++ api/app.js
@@ -20,8 +20,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
    
     res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header( "Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
   });
